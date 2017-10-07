222dd41cc4a576887e82e36c65b23f87e71db069
William Gough <williamhenrygough@gmail.com>
2017-10-07 11:58:58 +0100

    Create Item POJO

---
+++ src/main/java/com/github/williamhgough/scrapey/model/Item.java
@@ -0,0 +1,47 @@
+package com.github.williamhgough.scrapey.model;
+
+public class Item {
+    private String title;
+    private String description;
+    private int unitPrice;
+    private int kcalPer100g;
+
+    public Item(String title, String description, int unitPrice, int kcalPer100g) {
+        this.title = title;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.kcalPer100g = kcalPer100g;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getKcalPer100g() {
+        return kcalPer100g;
+    }
+
+    public void setKcalPer100g(int kcalPer100g) {
+        this.kcalPer100g = kcalPer100g;
+    }
+}
