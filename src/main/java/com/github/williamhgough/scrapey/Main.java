b99278331cb12ef34dde7fa1d58b9b7881ac5753
William Gough <williamhenrygough@gmail.com>
2017-10-08 12:17:45 +0100

    Remove redundant variable initialiser

--- src/main/java/com/github/williamhgough/scrapey/Main.java
+++ src/main/java/com/github/williamhgough/scrapey/Main.java
@@ -2,15 +2,14 @@ package com.github.williamhgough.scrapey;
 
 import com.github.williamhgough.scrapey.model.Item;
 import com.github.williamhgough.scrapey.service.Scraper;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Item> items = new ArrayList<>();
+        List<Item> items;
 
         Scraper scraper = new Scraper(args[0]);
         items = scraper.fetchAllItems();
+        System.out.println(items);
     }
 }
